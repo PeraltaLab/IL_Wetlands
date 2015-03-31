69feb66949727332238f46beb837ed53294ff91f
Mario Muscarella <mmuscare@indiana.edu>
2015-03-31 11:26:41 -0400

    fixed a few typos that caused job to end

--- mothur/IL_BacBatch_A.sh
+++ mothur/IL_BacBatch_A.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=1:ppn=32,vmem=250gb,walltime=24:00:00
-#PBS -M alperalt@indiana.edu,mmuscare@indiana.edu
+#PBS -l nodes=1:ppn=10,vmem=250gb,walltime=24:00:00
+#PBS -M mmuscare@indiana.edu
 #PBS -m abe
 #PBS -j oe
 cd /N/dc2/projects/Lennon_Sequences/Peralta_IL_Wetlands
